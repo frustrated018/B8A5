4e587d6af27249a6bc1b32c11460ad4eef7773ca
frustrated018 <niloyd018@gmail.com>
2023-08-21 22:23:10 +0600

    Fianl touches to the JS

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,10 +1,18 @@
+// Constants that repeat multiple times
+const totalAmount = document.getElementById("total");
+const discountField = document.getElementById("discount");
+const grandTotalAmount = document.getElementById("grand-total");
+const entryPoint = document.getElementById("item-list");
+const makePurchaseBtn = document.getElementById("make-purchase");
+const cuponCodeInput = document.getElementById("cupon-code-input");
+const applyButton = document.getElementById("btn-apply");
+
 // Adding to Item list function
 let entryCounter = 1;
 function addToItemList(itemID) {
   const itemName = document
     .getElementById(itemID)
     .querySelector("h2").innerText;
-  const entryPoint = document.getElementById("item-list");
   const entry = document.createElement("div");
   entry.innerText = `${entryCounter}. ${itemName}`;
   entryCounter++;
@@ -17,8 +25,6 @@ function calculation(itemID) {
   const itemValue = parseFloat(
     document.getElementById(itemID).querySelector("span").innerText
   );
-  const totalAmount = document.getElementById("total");
-  const grandTotalAmount = document.getElementById("grand-total");
 
   //   calculation
   const newTotal = itemValue + parseFloat(totalAmount.innerText);
@@ -28,30 +34,22 @@ function calculation(itemID) {
   grandTotalAmount.innerText = newGrandTotal.toFixed(2);
 
   //   Activating apply button
-  const applyButton = document.getElementById("btn-apply");
   if (parseFloat(totalAmount.innerText) > 200) {
     applyButton.removeAttribute("disabled");
-  } else {
-    applyButton.setAttribute("disabled", "true");
   }
 
   // Activating the Make purchase button
-  const makePurchaseBtn = document.getElementById("make-purchase");
-  
+
   if (parseFloat(totalAmount.innerText) > 0) {
     makePurchaseBtn.removeAttribute("disabled");
   }
 }
 
-
 // Apply button click
 
 function cuponApply() {
-  const cuponCodeInput = document.getElementById("cupon-code-input");
   const cuponCode = cuponCodeInput.value;
   const totalAmount = document.getElementById("total").innerText;
-  const discountField = document.getElementById("discount");
-  const grandTotalAmount = document.getElementById("grand-total");
 
   // Cupon validation
   if (cuponCode === "SELL200") {
@@ -73,24 +71,19 @@ function cuponApply() {
   }
 }
 
-// Make Purchase button and Modal close Function
+// Modal close Function
 
 function goHome() {
-  const totalAmount = document.getElementById("total");
-  const discountField = document.getElementById("discount");
-  const grandTotalAmount = document.getElementById("grand-total");
-  const entryPoint = document.getElementById("item-list");
-  const makePurchaseBtn = document.getElementById("make-purchase");
   totalAmount.innerText = "00.00";
   discountField.innerText = "00.00";
   grandTotalAmount.innerText = "00.00";
-  entryPoint.innerHTML = '';
-  makePurchaseBtn.setAttribute('disabled', 'true');
+  entryPoint.innerHTML = "";
+  makePurchaseBtn.setAttribute("disabled", "true");
+  applyButton.setAttribute("disabled", "true");
 }
 
 // Sell 200 button click
 
 function promo() {
-  const cuponCodeInput = document.getElementById("cupon-code-input");
-  cuponCodeInput.value = 'SELL200';
+  cuponCodeInput.value = "SELL200";
 }
